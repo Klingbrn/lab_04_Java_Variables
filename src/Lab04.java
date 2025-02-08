58acd4f0fd156faf7eba58d20538ceb754be686a
klingbrn <klingbrn@mail.uc.edu>
2025-02-07 19:10:16 -0500

    Lab 04

--- src/Lab04.java
+++ src/Lab04.java
@@ -1,5 +1,49 @@
-public class Main {
+public class Lab04 {
     public static void main(String[] args) {
+        int intOperandA = 1;
+        int intOperandB = 3;
+        int intSum;
+        int intProduct;
+        int intDifference;
+        int intQuotient;
+        int intModulo;
+        double doubleOperandA = 2.0;
+        double doubleOperandB = 3.1;
+        double doubleSum;
+        double doubleProduct;
+        double doubleDifference;
+        double doubleQuotient;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The Sum of intOperandA and intOperandB is: " + intSum);
+
+        intDifference = intOperandA -intOperandB ;
+        System.out.println("The Difference of intOperandA and intOperandB is: " + intDifference);
+
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The Product of intOperandA and intOperandB is: " + intProduct);
+
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The Quotient of intOperandA and intOperandB is: " + intQuotient);
+
+        intModulo = intOperandA % intOperandB;
+        System.out.println("the Modulo of intOperandA and intOperandB is: " + intModulo);
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The Sum of doubleOperandA and doubleOperandB is: " + doubleSum);
+
+        doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The Difference of doubleOperandA and doubleOperandB is: " + doubleDifference);
+
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The Product of doubleOperandA and doubleOperandB is: " + doubleProduct);
+
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The Quotient of doubleOperandA and doubleOperandB is: " + doubleQuotient);
+
+
+
+
 
     }
-        int intOperandA = 1;
+}
